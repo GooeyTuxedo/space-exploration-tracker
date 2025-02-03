73bb7cb1ba28d791b4dadaa99ef981e431b9cdf4
MrTuxedo <dev@based.consulting>
2025-02-03 11:36:22 -0800

    add html parsing for mission details page

--- src/app/missions/[id]/page.tsx
+++ src/app/missions/[id]/page.tsx
@@ -1,11 +1,15 @@
+import parse from "html-react-parser"
 import { fetchProjectDetails } from "@/utils/nasa-api"
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 
-export default async function ProjectDetailsPage({ params }: { params: { id: string } }) {
+export default async function ProjectDetailsPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   try {
     const projectData = await fetchProjectDetails(Number.parseInt(params.id))
     const project = projectData.project
+
+    console.log(`project details: `, project)
 
     return (
       <div className="container mx-auto px-4 py-8">
@@ -23,17 +27,17 @@ export default async function ProjectDetailsPage({ params }: { params: { id: str
             <p className="mb-2">
               <strong>End Date:</strong> {project.endDate || "Not specified"}
             </p>
-            <p className="mb-4">
-              <strong>Description:</strong> {project.description || "No description available."}
-            </p>
+            <div className="mb-4">
+              <strong>Description:</strong> {parse(project.description) || "No description available."}
+            </div>
             {project.benefits && (
-              <p className="mb-4">
-                <strong>Benefits:</strong> {project.benefits}
-              </p>
+              <div className="mb-4">
+                <strong>Benefits:</strong> {parse(project.benefits)}
+              </div>
             )}
             {project.leadOrganization && (
               <p>
-                <strong>Lead Organization:</strong> {project.leadOrganization}
+                <strong>Lead Organization:</strong> {project.leadOrganization.organizationName}
               </p>
             )}
           </CardContent>
